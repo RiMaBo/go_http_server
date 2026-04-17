542318239f2f7c61efd5d07b257bfbf030cee336
RiMaBo <rimabo@gmail.com>
2026-04-17 17:29:07 -0600

    Server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Server struct {
+	httpServer http.Server
+}
+
+func NewServer(port string, serveMux *http.ServeMux) Server {
+	return Server{
+		httpServer: http.Server{
+			Addr:    ":" + port,
+			Handler: serveMux,
+		},
+	}
+}
+
+func main() {
+	port := "8080"
+	mux := http.NewServeMux()
+	s := NewServer(port, mux)
+
+	fmt.Printf("Serving on Port: %s\n", port)
+	if err := s.httpServer.ListenAndServe(); err != nil {
+		fmt.Errorf("Error Starting Server: %v", err)
+	}
+}
