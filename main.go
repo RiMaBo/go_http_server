1e16c47abc2469e859603557218e2622c4101bf8
RiMaBo <rimabo@gmail.com>
2026-04-18 14:13:00 -0600

    Filerservers

--- main.go
+++ main.go
@@ -9,21 +9,22 @@ type Server struct {
 	httpServer http.Server
 }
 
-func NewServer(port string, serveMux *http.ServeMux) Server {
+func NewServer(port string, mux *http.ServeMux) Server {
 	return Server{
 		httpServer: http.Server{
 			Addr:    ":" + port,
-			Handler: serveMux,
+			Handler: mux,
 		},
 	}
 }
 
 func main() {
-	port := "8080"
-	mux := http.NewServeMux()
-	s := NewServer(port, mux)
+	const port = "8080"
 
+	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	s := NewServer(port, mux)
 
 	fmt.Printf("Serving on Port: %s\n", port)
 	if err := s.httpServer.ListenAndServe(); err != nil {
